9bcca69ee70294cbd9b5e9c4122bb67ac9f3f4a5
Luuk Willemsen (Twinone) <twinonedevs@gmail.com>
2016-05-31 13:01:08 +0200

    Cropping working, allow to scale crop selector (by code)

--- assets/capture.js
+++ assets/capture.js
@@ -1,5 +1,10 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
+// Scale the cropping view to this percentage of the camera view
+var cropScale = 0.5;
+var vid = document.getElementById("capture-video");
+
+
 function displayVideo() {
   if (navigator.getUserMedia) {
     navigator.getUserMedia({video: true}, videoSuccess, videoError);
@@ -8,7 +13,6 @@ function displayVideo() {
 
 function videoSuccess(stream) {
   console.log("videosuccess");
-  var vid = document.getElementById("capture-video");
   vid.src = window.URL.createObjectURL(stream);
 
   vid.onloadedmetadata = function() {
@@ -16,11 +20,11 @@ function videoSuccess(stream) {
   }
 }
 
-function onVideoLoaded(vid) {
+function onVideoLoaded() {
 
   var w = vid.offsetWidth;
   var h = vid.offsetHeight;
-  var min = Math.min(w, h);
+  var min = Math.min(w, h) * cropScale;
   var dx = (w-min)/2;
   var dy = (h-min)/2;
 
@@ -29,7 +33,7 @@ function onVideoLoaded(vid) {
   canvas.height = h;
   var c = canvas.getContext("2d");
 
-  var lh = 5;
+  var lh = 3;
   c.strokeStyle = "#ffffff";
   c.lineWidth = lh;
 
@@ -42,14 +46,36 @@ function onVideoLoaded(vid) {
   c.rect(dx+min/3,dy+lh/2,min/3,min-lh);
   c.stroke();
 
+  canvas.addEventListener("click", function() {
+    capture();
+  });
+
 }
 
 function videoError(e) {
-  console.log("Permission denied");
+  console.log("Permission denied for camera");
 }
 
-function capture() {
 
+function capture() {
+  var w = vid.videoWidth;
+  var h = vid.videoHeight;
+  var s = Math.min(w, h) * cropScale;
+  var dx = (w-s)/2;
+  var dy = (h-s)/2;
+
+  console.log("capturing!");
+  var scale = 0.2;
+  var canvas = document.createElement("canvas");
+  canvas.width = s*scale;
+  canvas.height = s*scale;
+  canvas.getContext('2d')
+        .drawImage(vid, dx,dy,s,s,0,0,s*scale,s*scale);
+
+  var img = document.createElement("img");
+  img.src = canvas.toDataURL();
+  var images = document.getElementById("images");
+  images.insertBefore(img, images.childNodes[0]);
 }
 
 
