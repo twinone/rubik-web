07c4dd9b728f9c9abe739f78bbb5a33384c22a83
Luuk Willemsen (Twinone) <twinonedevs@gmail.com>
2016-05-31 15:42:34 +0200

    Capture avegares colors

--- assets/capture.js
+++ assets/capture.js
@@ -3,6 +3,8 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 // Scale the cropping view to this percentage of the camera view
 var cropScale = 0.5;
 var vid = document.getElementById("capture-video");
+var faces = [];
+
 
 
 function displayVideo() {
@@ -65,17 +67,72 @@ function capture() {
   var dy = (h-s)/2;
 
   console.log("capturing!");
-  var scale = 0.2;
+  var scale = 0.3;
   var canvas = document.createElement("canvas");
   canvas.width = s*scale;
   canvas.height = s*scale;
-  canvas.getContext('2d')
-        .drawImage(vid, dx,dy,s,s,0,0,s*scale,s*scale);
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(vid, dx,dy,s,s,0,0,s*scale,s*scale);
 
   var img = document.createElement("img");
   img.src = canvas.toDataURL();
   var images = document.getElementById("images");
   images.insertBefore(img, images.childNodes[0]);
+  faces.push(ctx.getImageData(0,0,s*scale,s*scale));
+  if (faces.length == 1) {
+    processFaces();
+  }
+}
+
+function processFaces() {
+  var state = [];
+  for (var i = 0; i < faces.length; i++) {
+      state.push(processFace(faces[i]));
+  }
+  faces = [];
+  console.log(state);
+}
+
+function px(img, x, y) {
+  var offset = (x+y*img.width)*4;
+  return {
+    'r': img.data[offset+0],
+    'g': img.data[offset+1],
+    'b': img.data[offset+2],
+    'a': img.data[offset+3],
+  }
+}
+
+function processFace(img) {
+  console.log("Processing", img);
+  var colors = [];
+  var bs = img.width/3;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      colors[i*3+j] = {'r': 0, 'g':0, 'b':0, 'a': 0};
+      for (var k = 0; k < bs; k++) {
+        for (var l = 0; l < bs; l++) {
+          pixel = px(img, j*bs+l, i*bs+k);
+          colors[i*3+j].r += pixel.r * pixel.r;
+          colors[i*3+j].g += pixel.g * pixel.g;
+          colors[i*3+j].b += pixel.b * pixel.b;
+          colors[i*3+j].a += pixel.a * pixel.a;
+        }
+      }
+      colors[i*3+j].r /= bs*bs;
+      colors[i*3+j].g /= bs*bs;
+      colors[i*3+j].b /= bs*bs;
+      colors[i*3+j].a /= bs*bs;
+    }
+  }
+
+  for (var i = 0; i < colors.length; i++) {
+    colors[i].r = Math.sqrt(colors[i].r);
+    colors[i].g = Math.sqrt(colors[i].g);
+    colors[i].b = Math.sqrt(colors[i].b);
+    colors[i].a = Math.sqrt(colors[i].a);
+  }
+  return colors;
 }
 
 
